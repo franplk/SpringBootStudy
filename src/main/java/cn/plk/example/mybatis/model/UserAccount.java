84c1702e5e779254168dd477a24a6121b2a8d770
franplk <franplk@126.com>
2017-04-27 08:43:57 +0800

    Add Mybatis Feature Support

---
+++ src/main/java/cn/plk/example/mybatis/model/UserAccount.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package cn.plk.example.mybatis.model;
+
+/**
+ * @author {康培亮/AB052634}
+ *
+ */
+public class UserAccount {
+	
+	private String username;
+	private String password;
+	private String mail;
+	private String telphone;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getTelphone() {
+		return telphone;
+	}
+
+	public void setTelphone(String telphone) {
+		this.telphone = telphone;
+	}
+
+}
