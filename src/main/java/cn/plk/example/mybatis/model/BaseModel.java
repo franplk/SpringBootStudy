8cb5d15d4960e4697797fc2e7107b5beb18dd298
franplk <franplk@126.com>
2017-05-04 11:18:33 +0800

    Mybatis Generator Feature

---
+++ src/main/java/cn/plk/example/mybatis/model/BaseModel.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package cn.plk.example.mybatis.model;
+
+import java.io.Serializable;
+
+/**
+ * @author {康培亮/AB052634}
+ *
+ */
+public class BaseModel implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
